983f80fa41b7d1020241feb87b21933eeb8d468a
gsk <171930433@qq.com>
2025-04-22 15:44:39 +0800

    使用c++11标准，测试用例通过

--- coordinate_converter.hpp
+++ coordinate_converter.hpp
@@ -3,124 +3,158 @@
 
 #include <Eigen/Core>
 #include <Eigen/Geometry>
-namespace coordinate_converter {
+#include <type_traits>
 
-constexpr double WGS84_ELLIPSOID_RE = 6378137.0;
-constexpr double WGS84_ELLIPSOID_F = (1.0 / 298.257223563);
+namespace coordinate_converter
+{
+  // 检测类型是否有Re和F静态成员
+  template <typename T, typename = void>
+  struct has_para_members : std::false_type
+  {
+  };
 
-template <double _Re, double _F>
-struct Ellipsoid {
-public:
-  Ellipsoid() = default;
-  Ellipsoid(Eigen::Vector3d const& origin) { SetOrigin(origin); }
+  template <typename T>
+  struct has_para_members<T, std::void_t<decltype(T::Re), decltype(T::F)>> : std::true_type
+  {
+  };
 
-  void SetOrigin(Eigen::Vector3d const& origin) {
-    origin_ecef_ = LLH2ECEF(origin);
-    origin_cne_ = Pos2Cne(origin);
-  }
+  struct WGS84Para
+  {
+    static constexpr double Re = 6378137.0;
+    static constexpr double F = (1.0 / 298.257223563);
+  };
+}
 
-public:
-  static constexpr void LLH2ECEF(double const* pos, double* xyz) {
-    double const& b = pos[0];
-    double const& l = pos[1];
-    double const& h = pos[2];
-    double const& n = N(b);
-    double const& cb = cos(b);
-    xyz[0] = (n + h) * cb * cos(l);
-    xyz[1] = (n + h) * cb * sin(l);
-    xyz[2] = (n * (1 - _e1 * _e1) + h) * sin(b);
-  }
+namespace coordinate_converter
+{
 
-  static constexpr void ECEF2LLH(double const* xyz, double* pos) {
-    double const e1_2 = _e1 * _e1;
-    double const r2 = std::hypot(xyz[0], xyz[1]);
-    double v = _a;
-    double z = xyz[2];
-    double sinp = 0;
-    for (double zk = 0; fabs(z - zk) >= 1e-4;) {
-      zk = z;
-      sinp = z / sqrt(r2 + z * z);
-      v = _a / sqrt(1 - e1_2 * sinp * sinp);
-      z = xyz[2] + v * e1_2 * sinp;
+  template <typename _Para>
+  class Ellipsoid
+  {
+    static_assert(has_para_members<_Para>::value, "Ellipsoid parameter must be a *Para");
+    static constexpr double _a = _Para::Re;
+    static constexpr double _f = _Para::F;
+    static constexpr double _b = (1 - _f) * _a;
+    static constexpr double _c = _a * _a / _b;
+    static constexpr double _e1 = sqrt(_a * _a - _b * _b) / _a;
+    static constexpr double _e2 = sqrt(_a * _a - _b * _b) / _b;
+
+  public:
+    Ellipsoid() = default;
+    Ellipsoid(Eigen::Vector3d const &origin) { SetOrigin(origin); }
+
+    void SetOrigin(Eigen::Vector3d const &origin)
+    {
+      origin_ecef_ = LLH2ECEF(origin);
+      origin_cne_ = Pos2Cne(origin);
     }
-    auto temp_x = xyz[2] > 0.0 ? M_PI / 2.0 : -M_PI / 2.0;
-    pos[0] = r2 > 1E-12 ? atan(z / sqrt(r2)) : temp_x;
-    pos[1] = r2 > 1E-12 ? atan2(xyz[1], xyz[0]) : 0.0;
-    pos[2] = sqrt(r2 + z * z) - v;
-  }
 
-  static Eigen::Matrix3d Pos2Cne(const Eigen::Vector3d& pos) {
-    using namespace Eigen;
-    double const& b = pos[0];
-    double const& l = pos[1];
-    Matrix3d re = (AngleAxisd(-(M_PI / 2 - b), Vector3d::UnitX()) * AngleAxisd(-(M_PI / 2 + l), Vector3d::UnitZ()))
-                    .toRotationMatrix();
-    return re;
-  }
+  public:
+    static constexpr void LLH2ECEF(double const *pos, double *xyz)
+    {
+      double const &b = pos[0];
+      double const &l = pos[1];
+      double const &h = pos[2];
+      double const &n = N(b);
+      double const &cb = cos(b);
+      xyz[0] = (n + h) * cb * cos(l);
+      xyz[1] = (n + h) * cb * sin(l);
+      xyz[2] = (n * (1 - _e1 * _e1) + h) * sin(b);
+    }
 
-  // eigen wrapper
-  static Eigen::Vector3d LLH2ECEF(const Eigen::Vector3d& pos) {
-    Eigen::Vector3d xyz = Eigen::Vector3d::Zero();
-    LLH2ECEF(pos.data(), xyz.data());
-    return xyz;
-  }
-  static Eigen::Vector3d ECEF2LLH(const Eigen::Vector3d& xyz) {
-    Eigen::Vector3d pos = Eigen::Vector3d::Zero();
-    ECEF2LLH(xyz.data(), pos.data());
-    return pos;
-  }
+    static constexpr void ECEF2LLH(double const *xyz, double *pos)
+    {
+      double const e1_2 = _e1 * _e1;
+      double const r2 = xyz[0] * xyz[0] + xyz[1] * xyz[1];
+      double v = _a;
+      double z = xyz[2];
+      double sinp = 0;
+      for (double zk = 0; fabs(z - zk) >= 1e-4;)
+      {
+        zk = z;
+        sinp = z / sqrt(r2 + z * z);
+        v = _a / sqrt(1 - e1_2 * sinp * sinp);
+        z = xyz[2] + v * e1_2 * sinp;
+      }
+      double temp_x = xyz[2] > 0.0 ? M_PI / 2.0 : -M_PI / 2.0;
+      pos[0] = r2 > 1E-12 ? atan(z / sqrt(r2)) : temp_x;
+      pos[1] = r2 > 1E-12 ? atan2(xyz[1], xyz[0]) : 0.0;
+      pos[2] = sqrt(r2 + z * z) - v;
+    }
 
-  static Eigen::Vector3d LLH2ENU(const Eigen::Vector3d& pos, const Eigen::Vector3d& origin) {
-    Eigen::Vector3d origin_ecef = LLH2ECEF(origin);
-    Eigen::Matrix3d c_ne = Pos2Cne(origin);
-    Eigen::Vector3d local_enu = c_ne * (LLH2ECEF(pos) - origin_ecef);
-    return local_enu;
-  }
+    static Eigen::Matrix3d Pos2Cne(const Eigen::Vector3d &pos)
+    {
+      using namespace Eigen;
+      double const &b = pos[0];
+      double const &l = pos[1];
+      Matrix3d re = (AngleAxisd(-(M_PI / 2 - b), Vector3d::UnitX()) * AngleAxisd(-(M_PI / 2 + l), Vector3d::UnitZ()))
+                        .toRotationMatrix();
+      return re;
+    }
 
-  Eigen::Vector3d LLH2ENU(const Eigen::Vector3d& pos) const {
-    assert(origin_cne_.isZero(1e-12));
-    return origin_cne_ * (LLH2ECEF(pos) - origin_ecef_);
-  }
-  Eigen::Vector3d ENU2LLh(const Eigen::Vector3d& pos) const {
-    assert(origin_cne_.isZero(1e-12));
-    return ECEF2LLH(origin_cne_ + origin_cne_.transpose() * pos);
-  }
+    // eigen wrapper
+    static Eigen::Vector3d LLH2ECEF(const Eigen::Vector3d &pos)
+    {
+      Eigen::Vector3d xyz = Eigen::Vector3d::Zero();
+      LLH2ECEF(pos.data(), xyz.data());
+      return xyz;
+    }
+    static Eigen::Vector3d ECEF2LLH(const Eigen::Vector3d &xyz)
+    {
+      Eigen::Vector3d pos = Eigen::Vector3d::Zero();
+      ECEF2LLH(xyz.data(), pos.data());
+      return pos;
+    }
 
-  static Eigen::Vector3d ENU2LLH(const Eigen::Vector3d& pos, const Eigen::Vector3d& origin) {
-    Eigen::Vector3d const origin_ecef = LLH2ECEF(origin);
-    Eigen::Matrix3d const c_ne = Pos2Cne(origin);
-    Eigen::Vector3d const pos_llh = ECEF2LLH(c_ne.transpose() * pos + origin_ecef);
-    return pos_llh;
-  }
+    static Eigen::Vector3d LLH2ENU(const Eigen::Vector3d &pos, const Eigen::Vector3d &origin)
+    {
+      Eigen::Vector3d origin_ecef = LLH2ECEF(origin);
+      Eigen::Matrix3d c_ne = Pos2Cne(origin);
+      Eigen::Vector3d local_enu = c_ne * (LLH2ECEF(pos) - origin_ecef);
+      return local_enu;
+    }
 
-  static Eigen::Isometry3d Tn0n1(const Eigen::Vector3d& pos, const Eigen::Vector3d& origin) {
-    using namespace Eigen;
-    Matrix3d const Cn0_e = Pos2Cne(origin);
-    Matrix3d const Cn1_e = Pos2Cne(pos);
-    Isometry3d const T_n0_n1 = Translation3d(LLH2ENU(pos, origin)) * Quaterniond(Cn0_e * Cn1_e.transpose());
-    return T_n0_n1;
-  }
+    Eigen::Vector3d LLH2ENU(const Eigen::Vector3d &pos) const
+    {
+      assert(!origin_ecef_.isZero(1e-12));
+      return origin_cne_ * (LLH2ECEF(pos) - origin_ecef_);
+    }
+    Eigen::Vector3d ENU2LLH(const Eigen::Vector3d &pos) const
+    {
+      assert(!origin_ecef_.isZero(1e-12));
+      return ECEF2LLH(origin_ecef_ + origin_cne_.transpose() * pos);
+    }
 
-public:
-  Eigen::Vector3d origin_ecef_{ 0, 0, 0 };
-  Eigen::Matrix3d origin_cne_ = Eigen::Matrix3d::Identity();
+    static Eigen::Vector3d ENU2LLH(const Eigen::Vector3d &pos, const Eigen::Vector3d &origin)
+    {
+      Eigen::Vector3d const origin_ecef = LLH2ECEF(origin);
+      Eigen::Matrix3d const c_ne = Pos2Cne(origin);
+      Eigen::Vector3d const pos_llh = ECEF2LLH(c_ne.transpose() * pos + origin_ecef);
+      return pos_llh;
+    }
 
-public:
-  static constexpr double _a = _Re;
-  static constexpr double _f = _F;
-  static constexpr double _b = (1 - _f) * _a;
-  static constexpr double _c = _a * _a / _b;
-  static constexpr double _e1 = sqrt(_a * _a - _b * _b) / _a;
-  static constexpr double _e2 = sqrt(_a * _a - _b * _b) / _b;
+    static Eigen::Isometry3d Tn0n1(const Eigen::Vector3d &pos, const Eigen::Vector3d &origin)
+    {
+      using namespace Eigen;
+      Matrix3d const Cn0_e = Pos2Cne(origin);
+      Matrix3d const Cn1_e = Pos2Cne(pos);
+      Isometry3d const T_n0_n1 = Translation3d(LLH2ENU(pos, origin)) * Quaterniond(Cn0_e * Cn1_e.transpose());
+      return T_n0_n1;
+    }
 
-  static constexpr double W(const double B_) { return sqrt(1 - pow(_e1 * sin(B_), 2)); }
-  static constexpr double V(const double B_) { return sqrt(1 + pow(_e2 * cos(B_), 2)); }
-  static constexpr double M(const double B_) { return _c / pow(V(B_), 3); } // 子午曲率半径
-  static constexpr double N(const double B_) { return _c / V(B_); }         // 卯酉曲率半径
-};
+  public:
+    Eigen::Vector3d origin_ecef_{0, 0, 0};
+    Eigen::Matrix3d origin_cne_ = Eigen::Matrix3d::Identity();
 
-using WGS84 = Ellipsoid<WGS84_ELLIPSOID_RE, WGS84_ELLIPSOID_F>;
+  public:
+    static constexpr double W(const double B_) { return sqrt(1 - pow(_e1 * sin(B_), 2)); }
+    static constexpr double V(const double B_) { return sqrt(1 + pow(_e2 * cos(B_), 2)); }
+    static constexpr double M(const double B_) { return _c / pow(V(B_), 3); } // 子午曲率半径
+    static constexpr double N(const double B_) { return _c / V(B_); }         // 卯酉曲率半径
+  };
+
+  using WGS84 = Ellipsoid<WGS84Para>;
 
 } // namespace coordinate_converter
 
-#endif // COORDINATE_CONVERTER_HPP 
+#endif // COORDINATE_CONVERTER_HPP
--- tests/earth.cpp
+++ tests/earth.cpp
@@ -1,36 +1,54 @@
 #include "coordinate_converter.hpp"
 #include <gtest/gtest.h>
-
+#include <iomanip>
 using namespace coordinate_converter;
-constexpr double dpr = 180.0 / M_PI;
-constexpr double rpd = M_PI / 180.0;
+constexpr double operator"" _deg(long double x) { return x / 180.0 * M_PI; }
+constexpr double operator"" _deg(unsigned long long x) { return x / 180.0 * M_PI; }
 
-TEST(Earth, base)
+TEST(Ellipsoid, type)
+{
+  EXPECT_TRUE(has_para_members<WGS84Para>::value);
+  EXPECT_FALSE(has_para_members<int>::value);
+}
+
+TEST(Ellipsoid, base)
 {
 
   WGS84 wgs84;
 
-  Eigen::Vector3d ecef_true = {801927, -5.13661e+06, 3.68258e+06};
-  Eigen::Vector3d llh{(120.0 * rpd), (30.0 * rpd), 0};
-  Eigen::Vector3d ecef = WGS84::LLH2ECEF(llh);
+  Eigen::Vector3d ecef_true = {-2764128.3196464167, 4787610.6882675830, 3170373.7353836372};
+  Eigen::Vector3d llh_true{30.0_deg, 120.0_deg, 0};
+  Eigen::Vector3d ecef = WGS84::LLH2ECEF(llh_true);
+  Eigen::Vector3d llh = WGS84::ECEF2LLH(ecef_true);
 
-  GTEST_LOG_(INFO) << llh.transpose();
-  GTEST_LOG_(INFO) << ecef.transpose();
+  // GTEST_LOG_(INFO) << std::setprecision(10) << std::fixed << ecef_true.transpose();
+  // GTEST_LOG_(INFO) << std::setprecision(10) << std::fixed << llh_true.transpose();
+  // GTEST_LOG_(INFO) << std::setprecision(10) << std::fixed << ecef.transpose();
+  // GTEST_LOG_(INFO) << std::setprecision(10) << std::fixed << llh.transpose();
 
   EXPECT_TRUE(ecef_true.isApprox(ecef, 1e-6));
+  EXPECT_TRUE(llh_true.isApprox(llh, 1e-6));
 }
 
-TEST(Earth, origin)
+TEST(Ellipsoid, origin)
 {
 
-  Eigen::Vector3d origin{(120.0 * rpd), (30.0 * rpd), 0};
+  Eigen::Vector3d origin{30.0_deg, 120.0_deg, 0};
   WGS84 wgs84(origin);
 
-  Eigen::Vector3d enu_true = {25901.5, 31823, -122.186};
-  Eigen::Vector3d llh{(120.005 * rpd), (30.005 * rpd), 10};
-  Eigen::Vector3d enu = wgs84.LLH2ENU(llh);
+  Eigen::Vector3d enu_true = {0, 10, 0};
+  Eigen::Vector3d llh_true{0.5236003501, 2.0943951024, 0.0000078734};
 
-  GTEST_LOG_(INFO) << enu.transpose();
+  Eigen::Vector3d enu = wgs84.LLH2ENU(llh_true);
+  Eigen::Vector3d llh = wgs84.ENU2LLH(enu_true);
 
-  EXPECT_TRUE(enu_true.isApprox(enu, 1e-6));
+  GTEST_LOG_(INFO) << "origin " << std::setprecision(10) << std::fixed << (origin).transpose();
+  GTEST_LOG_(INFO) << "llh_true " << std::setprecision(10) << std::fixed << (llh_true).transpose();
+  GTEST_LOG_(INFO) << "enu_true " << std::setprecision(10) << std::fixed << (enu_true).transpose();
+  GTEST_LOG_(INFO) << "enu 2 llh " << std::setprecision(10) << std::fixed << (llh).transpose();
+  GTEST_LOG_(INFO) << "llh 2 enu " << std::setprecision(10) << std::fixed << (enu).transpose();
+
+  EXPECT_TRUE(enu_true.isApprox(enu, 1e-4));
+  EXPECT_TRUE(llh_true.isApprox(llh, 1e-8));
+  EXPECT_TRUE(1);
 }
