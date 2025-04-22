0d5eacd2daa9227bccb07e07d472f118fb9bd5b2
gsk <171930433@qq.com>
2025-04-22 10:26:34 +0800

    初始提交

---
+++ coordinate_converter.hpp
@@ -0,0 +1,18 @@
+#ifndef COORDINATE_CONVERTER_HPP
+#define COORDINATE_CONVERTER_HPP
+
+#if defined(_MSC_VER)
+    #define COORDINATE_CONVERTER_EXPORT __declspec(dllexport)
+#else
+    #define COORDINATE_CONVERTER_EXPORT
+#endif
+
+#include <Eigen/Core>
+
+namespace coordinate_converter {
+
+// 在这里添加你的坐标转换函数和类
+
+} // namespace coordinate_converter
+
+#endif // COORDINATE_CONVERTER_HPP 
